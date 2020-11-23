d1032e32d80cc454259973382ecb70ca01afd67a
Maycie Morris <maycie.morris@gmail.com>
2020-11-23 17:44:59 -0500

    Dynamic rating

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -1,12 +1,13 @@
 import React from 'react';
 import { data } from './data';
+import { Product } from './components/Product'
 
 function App() {
   return (
     <div classNameName="grid-container">
       <header className="row">
         <div>
-          <a className="brand" href="/"><img id="wow_logo" src="./images/wow_logo.png" /></a>
+          <a className="brand" href="/"><img id="wow_logo" src="/images/wow_logo.png" /></a>
         </div>
         <div>
           <a href="/cart">Cart</a>
@@ -18,26 +19,7 @@ function App() {
           <div className="row center">
               {
                 data.products.map(product => (
-                  <div key={ product.id } className="card">
-                  <a href= {`/product/${product._id}`} >
-                    <img className="medium" src={ product.image } alt={ product.name } />
-                  </a>
-                  <div className="card-body">
-                    <a href= {`/product/${product._id}`} >
-                      <h2> { product.name } </h2>
-                    </a>
-                    <div className="rating">
-                      <span> <i className="fa fa-star"></i> </span>
-                      <span> <i className="fa fa-star"></i> </span>
-                      <span> <i className="fa fa-star"></i> </span>
-                      <span> <i className="fa fa-star"></i> </span>
-                      <span> <i className="fa fa-star-o"></i> </span>
-                    </div>
-                  <div className="price">
-                    ${product.price}
-                  </div>
-                  </div>
-                </div>
+                  <Product product={ product } />
                 ))
               }
           </div>
