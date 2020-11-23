3781f47f1616739a7bbd8e9b6d4f9e0f149145b8
Maycie Morris <maycie.morris@gmail.com>
2020-11-23 17:23:18 -0500

    Dynamic product cards

---
+++ frontend/src/data.js
@@ -0,0 +1,70 @@
+export const data = {
+    products: [
+        {
+            _id: `1`,
+            name: `Pumpkin Sweater`,
+            category: `Tops`,
+            image: `/images/product-1.jpg`,
+            price: 20,
+            brand: `WOW_co`,
+            rating: `4.5`,
+            numReviews: 10,
+            description: `Comfy, cozy sweater`
+        },
+        {
+            _id: `2`,
+            name: `Gangsta Wrapper Shirt`,
+            category: `Tops`,
+            image: `/images/product-2.jpg`,
+            price: 15,
+            brand: `WOW_co`,
+            rating: `4.5`,
+            numReviews: 3,
+            description: `Perfect shirt for a serious gift wrapper who knows what they're doing`
+        },
+        {
+            _id: `3`,
+            name: `Custom Sign`,
+            category: `Decorations`,
+            image: `/images/product-3.jpg`,
+            price: 15,
+            brand: `WOW_co`,
+            rating: `4.8`,
+            numReviews: 10,
+            description: `A custom sign for any occasion`
+        },
+        {
+            _id: `4`,
+            name: `Hocus Pocus Mug`,
+            category: `Homeware`,
+            image: `/images/product-4.jpg`,
+            price: 10,
+            brand: `WOW_co`,
+            rating: `5`,
+            numReviews: 19,
+            description: `Perfect for the spooky season!`
+        },
+        {
+            _id: `5`,
+            name: `WOW_co Hat`,
+            category: `Hats`,
+            image: `/images/product-5.jpg`,
+            price: 18,
+            brand: `WOW_co`,
+            rating: `4.3`,
+            numReviews: 8,
+            description: `Rep WOW_co with the perfect, comfy hat!`
+        },
+        {
+            _id: `6`,
+            name: `Tye-Dye Sweatpants`,
+            category: `Bottoms`,
+            image: `/images/product-6.jpg`,
+            price: 20,
+            brand: `WOW_co`,
+            rating: `4.5`,
+            numReviews: 10,
+            description: `Super warm, super stylish, made to order tye-dye sweatpants!`
+        },
+    ]
+}
