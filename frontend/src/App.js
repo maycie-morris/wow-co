24e7b5ccada2ef4d6e5d483af73e25c972291625
Maycie Morris <maycie.morris@gmail.com>
2020-11-26 21:03:03 -0500

    sign in & sign out

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -3,13 +3,23 @@ import { BrowserRouter, Link, Route } from 'react-router-dom';
 import { ProductScreen } from './screens/ProductScreen';
 import { HomeScreen } from './screens/HomeScreen'
 import { CartScreen } from './screens/CartScreen';
-import { useSelector } from 'react-redux';
-import { SignInScreen } from './screens/SignInScreen';
+import { useDispatch, useSelector } from 'react-redux';
+import { SigninScreen } from './screens/SignInScreen';
+import { signout } from './actions/userActions'
 
 function App() {
 
   const cart = useSelector(state => state.cart);
   const { cartItems } = cart;
+
+  const userSignin = useSelector(state => state.userSignin);
+  const { userInfo } = userSignin;
+
+  const dispatch = useDispatch();
+
+  const signoutHandler = () => {
+    dispatch(signout());
+  };
 
   return (
     <BrowserRouter>
@@ -24,13 +34,28 @@ function App() {
             <span className="badge"> { cartItems.length } </span>
           )}
           </Link>
-          <Link to="/signin">Sign In</Link>
+          {userInfo ? (
+              <div className="dropdown">
+                <Link to="#">
+                  {userInfo.name} <i className="fa fa-caret-down"></i>{' '}
+                </Link>
+                <ul className="dropdown-content">
+                  <li>
+                    <Link to="#signout" onClick={signoutHandler}>
+                      Sign Out
+                    </Link>
+                  </li>
+                </ul>
+              </div>
+            ) : (
+              <Link to="/signin">Sign In</Link>
+            )}
         </div>
       </header>
       <main>
         <Route path='/cart/:id?' component={ CartScreen } />
         <Route path="/product/:id" component={ ProductScreen } />
-        <Route path='/signin' component={ SignInScreen } />
+        <Route path='/signin' component={ SigninScreen } />
         <Route path="/" component={ HomeScreen } exact />
       </main>
       {/* <footer className="row center">All rights reserved</footer> */}
