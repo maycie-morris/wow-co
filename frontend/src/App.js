133db2960a252cdd98f2f71aaaba4c4474f57cdc
Maycie Morris <maycie.morris@gmail.com>
2020-11-28 08:59:39 -0500

    Order summary page

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -9,6 +9,7 @@ import { signout } from './actions/userActions'
 import { RegisterScreen } from './screens/RegisterScreen';
 import { ShippingAddressScreen } from './screens/ShippingAddressScreen';
 import { PaymentMethodScreen } from './screens/PaymentMethodScreen';
+import { PlaceOrderScreen } from './screens/PlaceOrderScreen';
 
 function App() {
 
@@ -62,6 +63,7 @@ function App() {
         <Route path='/register' component={ RegisterScreen } />
         <Route path='/shipping' component={ ShippingAddressScreen } />
         <Route path='/payment' component={ PaymentMethodScreen } />
+        <Route path='/placeorder' component={ PlaceOrderScreen } />
         <Route path="/" component={ HomeScreen } exact />
       </main>
       {/* <footer className="row center">All rights reserved</footer> */}
