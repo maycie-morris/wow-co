77b06633ee7779212e0dd57000479284765e05e4
Maycie Morris <maycie.morris@gmail.com>
2020-11-29 14:04:46 -0500

    Paypal payment successful

--- backend/routers/orderRouter.js
+++ backend/routers/orderRouter.js
@@ -26,9 +26,47 @@ orderRouter.post(
       const createdOrder = await order.save();
       res
         .status(201)
-        .send({ message: 'New Order Created', order: createdOrder });
+        .send({ 
+          message: 'New Order Created', 
+          order: createdOrder 
+        });
     }
   })
 );
+
+
+orderRouter.get('/:id', isAuth, expressAsyncHandler(async(req, res) => {
+  const order = await Order.findById(req.params.id);
+  if(order) {
+    res.send(order);
+  } else {
+    res.status(404).send({
+      message: 'Order Not Found'
+    })
+  }
+}));
+
+
+orderRouter.put('/:id/pay', isAuth, expressAsyncHandler(async (req, res) => {
+  const order = await Order.findById(req.params.id);
+  if(order) {
+    order.isPaid = true;
+    order.paidAt = Date.now();
+    order.paymentResult = { 
+      id: req.body.id, 
+      status: req.body.status, 
+      update_time: req.body.update_time, 
+      email_address: req.body.email_address, 
+    };
+    const updateorder = await order.save();
+    res.send({
+      message: 'Order Paid', order: updatedOrder
+    });
+  } else {
+    res.status(404).send({
+      message: 'Order Not Found'
+    });
+  }
+}))
 
 export default orderRouter;
