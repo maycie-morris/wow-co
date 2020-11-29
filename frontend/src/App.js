3aba5394c4737846e9c742b375bd848ab0c9b184
Maycie Morris <maycie.morris@gmail.com>
2020-11-29 14:48:11 -0500

    Order history page

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -11,6 +11,7 @@ import { ShippingAddressScreen } from './screens/ShippingAddressScreen';
 import { PaymentMethodScreen } from './screens/PaymentMethodScreen';
 import { PlaceOrderScreen } from './screens/PlaceOrderScreen';
 import { OrderScreen } from './screens/OrderScreen';
+import { OrderHistoryScreen } from './screens/OrderHistoryScreen';
 
 function App() {
 
@@ -46,6 +47,9 @@ function App() {
                 </Link>
                 <ul className="dropdown-content">
                   <li>
+                    <Link to='/orderhistory'>Order History</Link>
+                  </li>
+                  <li>
                     <Link to="#signout" onClick={signoutHandler}>
                       Sign Out
                     </Link>
@@ -66,6 +70,7 @@ function App() {
         <Route path='/payment' component={ PaymentMethodScreen } />
         <Route path='/placeorder' component={ PlaceOrderScreen } />
         <Route path='/order/:id' component = { OrderScreen } />
+        <Route path='/orderhistory' component={ OrderHistoryScreen } />
         <Route path="/" component={ HomeScreen } exact />
       </main>
       {/* <footer className="row center">All rights reserved</footer> */}
