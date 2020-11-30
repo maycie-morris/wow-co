07179aeadee0449b7c15a69c7b17eb2ad1555260
Maycie Morris <maycie.morris@gmail.com>
2020-11-29 21:14:19 -0500

    Product editing screen

--- frontend/src/screens/ProductListScreen.js
+++ frontend/src/screens/ProductListScreen.js
@@ -10,93 +10,93 @@ import { productCreateReducer } from '../reducers/productReducers';
 
 export const ProductListScreen = (props) => {
 
-    const productList = useSelector((state) => state.productList);
-    const { loading, error, products } = productList;
+  const productList = useSelector((state) => state.productList);
+  const { loading, error, products } = productList;
 
-    const productCreate = useSelector(state => state.productCreate);
-    const { 
-      loading: loadingCreate,
-      error: errorCreate,
-      success: successCreate,
-      product: createdProduct
-     } = productCreateReducer;
+  const productCreate = useSelector((state) => state.productCreate);
+  const {
+    loading: loadingCreate,
+    error: errorCreate,
+    success: successCreate,
+    product: createdProduct,
+  } = productCreate;
 
-    const dispatch = useDispatch();
+  const dispatch = useDispatch();
 
-    useEffect(() => {
-      if(successCreate) {
-        dispatch({ type: PRODUCT_CREATE_RESET });
-        props.history.push(`/product/${createdProduct._id}/edit`)
-      }
-      dispatch(listProducts());
-    }, [createdProduct, dispatch, props.history, successCreate]);
-    
-
-    const deleteHandler = () => {
-      
-    };
-
-    const createHandler = () => {
-      dispatch(createProduct());
+  useEffect(() => {
+    if (successCreate) {
+      dispatch({ type: PRODUCT_CREATE_RESET });
+      props.history.push(`/product/${createdProduct._id}/edit`);
     }
+  }, [createdProduct, dispatch, props.history, successCreate]);
 
-    return (
-      <div>
-        <div className="row">
+  const deleteHandler = (product) => {
+  };
+
+
+  const createHandler = () => {
+    dispatch(createProduct());
+  };
+
+
+  return (
+    <div>
+      <div className="row">
         <h1>Products</h1>
         <button type="button" className="primary" onClick={createHandler}>
-          Add New Product
+          Create Product
         </button>
-        </div>
-        {loadingCreate && <LoadingBox />}
-        {errorCreate && <MessageBox variant="danger"> {errorCreate} </MessageBox>}
-        {loading ? (
-          <LoadingBox></LoadingBox>
-        ) : error ? (
-          <MessageBox variant="danger">{error}</MessageBox>
-        ) : (
-          <table className="table">
-            <thead>
-              <tr>
-                <th>ID</th>
-                <th>NAME</th>
-                <th>PRICE</th>
-                <th>CATEGORY</th>
-                <th>BRAND</th>
-                <th>ACTIONS</th>
-              </tr>
-            </thead>
-            <tbody>
-              {products.map((product) => (
-                <tr key={product._id}>
-                  <td>{product._id}</td>
-                  <td>{product.name}</td>
-                  <td>{product.price}</td>
-                  <td>{product.category}</td>
-                  <td>{product.brand}</td>
-                  <td>
-                    <button
-                      type="button"
-                      className="small"
-                      onClick={() =>
-                        props.history.push(`/product/${product._id}/edit`)
-                      }
-                    >
-                      Edit
-                    </button>
-                    <button
-                      type="button"
-                      className="small"
-                      onClick={() => deleteHandler(product)}
-                    >
-                      Delete
-                    </button>
-                  </td>
-                </tr>
-              ))}
-            </tbody>
-          </table>
-        )}
       </div>
-    );
-  }
+
+      {loadingCreate && <LoadingBox></LoadingBox>}
+      {errorCreate && <MessageBox variant="danger">{errorCreate}</MessageBox>}
+      {loading ? (
+        <LoadingBox></LoadingBox>
+      ) : error ? (
+        <MessageBox variant="danger">{error}</MessageBox>
+      ) : (
+        <table className="table">
+          <thead>
+            <tr>
+              <th>ID</th>
+              <th>NAME</th>
+              <th>PRICE</th>
+              <th>CATEGORY</th>
+              <th>BRAND</th>
+              <th>ACTIONS</th>
+            </tr>
+          </thead>
+          <tbody>
+            {products.map((product) => (
+              <tr key={product._id}>
+                <td>{product._id}</td>
+                <td>{product.name}</td>
+                <td>{product.price}</td>
+                <td>{product.category}</td>
+                <td>{product.brand}</td>
+                <td>
+                  <button
+                    type="button"
+                    className="small"
+                    onClick={() =>
+                      props.history.push(`/product/${product._id}/edit`)
+                    }
+                  >
+                    Edit
+                  </button>
+                  <button
+                    type="button"
+                    className="small"
+                    onClick={() => deleteHandler(product)}
+                  >
+                    Delete
+                  </button>
+                </td>
+              </tr>
+            ))}
+          </tbody>
+        </table>
+      )}
+    </div>
+  );
+}
