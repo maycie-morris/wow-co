c2bd12c6452b826d5961e7a19582cff467e6f1ba
Maycie Morris <maycie.morris@gmail.com>
2020-11-30 13:58:22 -0500

    Delivery

--- frontend/src/screens/OrderScreen.js
+++ frontend/src/screens/OrderScreen.js
@@ -2,11 +2,11 @@ import React, { useEffect, useState } from 'react';
 import { PayPalButton } from 'react-paypal-button-v2';
 import { Link } from 'react-router-dom';
 import { useDispatch, useSelector } from 'react-redux';
-import { detailsOrder, payOrder } from '../actions/orderActions';
+import { deliverOrder, detailsOrder, payOrder } from '../actions/orderActions';
 import { LoadingBox } from '../components/LoadingBox';
 import { MessageBox } from '../components/MessageBox';
 import Axios from 'axios';
-import { ORDER_PAY_RESET } from '../constants/orderConstants';
+import { ORDER_DELIVER_RESET, ORDER_PAY_RESET } from '../constants/orderConstants';
 
 
 export const OrderScreen = (props) => {
@@ -16,12 +16,22 @@ export const OrderScreen = (props) => {
     const orderDetails = useSelector(state => state.orderDetails);
     const { order, loading, error } = orderDetails;
 
+    const userSignin = useSelector((state) => state.userSignin);
+    const { userInfo } = userSignin;
+
     const orderPay = useSelector(state => state.orderPay);
     const { 
         loading: loadingPay,
         error: errorPay, 
         success: successPay
      } = orderPay;
+
+     const orderDeliver = useSelector((state) => state.orderDeliver);
+     const {
+       loading: loadingDeliver,
+       error: errorDeliver,
+       success: successDeliver,
+     } = orderDeliver;
 
     const dispatch = useDispatch();
 
@@ -37,8 +47,14 @@ export const OrderScreen = (props) => {
             };
             document.body.appendChild(script);
         };
-        if(!order || successPay || (order && order._id != orderId)) {
+        if (
+            !order ||
+            successPay ||
+            successDeliver ||
+            (order && order._id !== orderId)
+          ) {
             dispatch({ type: ORDER_PAY_RESET });
+            dispatch({ type: ORDER_DELIVER_RESET });
             dispatch(detailsOrder(orderId));
         } else {
             if(!order.isPaid) {
@@ -49,12 +65,17 @@ export const OrderScreen = (props) => {
                 }
             }
         }
-    }, [dispatch, order, orderId, sdkReady, successPay]);
+    }, [dispatch, order, orderId, sdkReady, successPay, successDeliver]);
 
 
     const successPaymentHandler = (paymentResult) => {
         dispatch(payOrder(order, paymentResult))
     };
+
+
+    const deliverHandler = () => {
+        dispatch(deliverOrder(order._id));
+      };
 
     return loading
         ? (<LoadingBox />) 
@@ -178,6 +199,21 @@ export const OrderScreen = (props) => {
                             </li>
                         )
                     }
+                    {userInfo.isAdmin && order.isPaid && !order.isDelivered && (
+                        <li>
+                        {loadingDeliver && <LoadingBox></LoadingBox>}
+                        {errorDeliver && (
+                            <MessageBox variant="danger">{errorDeliver}</MessageBox>
+                        )}
+                        <button
+                            type="button"
+                            className="primary block"
+                            onClick={deliverHandler}
+                        >
+                            Deliver Order
+                        </button>
+                        </li>
+              )}
                     </ul>
                 </div>
                 </div>
