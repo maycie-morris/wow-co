613fb24a1af6185f71c5e995b0ada9640dd013f1
Maycie Morris <maycie.morris@gmail.com>
2020-11-30 21:22:34 -0500

    Fixed textarea css

--- frontend/src/screens/ProductScreen.js
+++ frontend/src/screens/ProductScreen.js
@@ -49,7 +49,7 @@ export const ProductScreen = (props) => {
                     numReviews={product.numReviews}
                   ></Rating>
                 </li>
-                <li>Price : ${product.price}</li>
+                <li>Price: ${product.price}</li>
                 <li>
                   Description:
                   <p>{product.description}</p>
