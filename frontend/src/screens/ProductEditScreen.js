66258029a0082cd2e290442460b816c1530c9e06
Maycie Morris <maycie.morris@gmail.com>
2021-01-04 19:17:52 -0500

    Setting up AWS

--- frontend/src/screens/ProductEditScreen.js
+++ frontend/src/screens/ProductEditScreen.js
@@ -76,7 +76,7 @@ export const ProductEditScreen = (props) => {
         bodyFormData.append('image', file);
         setLoadingUpload(true);
         try {
-          const { data } = await Axios.post('/api/uploads', bodyFormData, {
+          const { data } = await Axios.post('/api/uploads/s3', bodyFormData, {
             headers: {
               'Content-Type': 'multipart/form-data',
               Authorization: `Bearer ${userInfo.token}`,
